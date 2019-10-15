311a0ac4c3fe9eb3da72176669da6840ca76aa13
unknown <petrides.anthony@gmail.com>
2019-10-15 19:09:01 +0100

    functions and arguments

---
+++ js-bootcamp/functions/arguments.js
@@ -0,0 +1,26 @@
+// Multiple args
+let add = function (a, b, c) {
+    return a + b + c
+}
+
+let result = add(10, 1, 5)
+console.log(result)
+
+// Default arguments
+    let getScoreText = function (name = 'Anonymous', score = 0) {
+        return 'Name: ' + name + ' - Score: ' + score
+}
+
+let scoreText = getScoreText(undefined, 99)
+console.log(scoreText)
+
+// Challenge area
+console.log('\n**** Challenge Area ****\n')
+
+// total, tipPercent .2 as default (20%)
+let tipCalculation = function (total, tipPercent = 0.2) {
+    return 'Paid £' + total + ' - Tipped £' + (tipPercent * total)
+}
+
+let totalTip = tipCalculation(100)
+console.log(totalTip)
