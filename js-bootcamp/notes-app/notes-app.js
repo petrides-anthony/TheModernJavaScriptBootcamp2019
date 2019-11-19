8afe9232499850baca8adf5e83238987c66f45b4
unknown <petrides.anthony@gmail.com>
2019-11-19 20:58:37 +0000

    Finished Section 8: Lecture: 75

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -33,3 +33,16 @@ window.addEventListener('storage', function (e) {
         renderNotes(notes, filters)
     }
 })
+
+// const now = moment()
+// now.subtract(1, 'week').subtract(20, 'days')
+// console.log(now.format('MMMM Do, YYYY'))
+// //November 3rd, 2003
+// console.log(now.fromNow())
+// const nowTimestamp = now.valueOf()
+
+// console.log(moment(nowTimestamp).toString())
+
+const birthday = moment()
+birthday.year(1991).month(11).date(18)
+console.log(birthday.format('MMM D, YYYY'))
