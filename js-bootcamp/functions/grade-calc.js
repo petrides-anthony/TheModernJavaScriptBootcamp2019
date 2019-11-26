7d530b72e33e805789e0809a8398cb6bbbe29213
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-11-26 08:52:04 +0000

    Finished Section 9: Lecture 86

--- js-bootcamp/functions/grade-calc.js
+++ js-bootcamp/functions/grade-calc.js
@@ -3,23 +3,32 @@
 // A 90-100%, B 80-89%, C 70-79%, D 60-69%, F 0-59%
 
 const studentGrade = function (studentScore, totalPossibleScore) {
-    const percent = (studentScore / totalPossibleScore) * 100
-    let gradeLetter = ''
-
-    if (percent >= 90) {
-        gradeLetter = 'A'
-    } else if (percent >= 80) {
-        gradeLetter = 'B'
-    } else if (percent >= 70) {
-        gradeLetter = 'C'
-    } else if (percent >= 60) {
-        gradeLetter = 'D'
-    } else {
-        gradeLetter = 'F'
+    if (typeof studentScore !== 'number' || typeof totalPossibleScore !== 'number') {
+        throw Error('Score must be calculated with a numeric value')
     }
 
-    return `You got a ${gradeLetter} (${percent}%)`
+    const percent = (studentScore / totalPossibleScore) * 100
+        let gradeLetter = ''
+
+        if (percent >= 90) {
+            gradeLetter = 'A'
+        } else if (percent >= 80) {
+            gradeLetter = 'B'
+        } else if (percent >= 70) {
+            gradeLetter = 'C'
+        } else if (percent >= 60) {
+            gradeLetter = 'D'
+        } else {
+            gradeLetter = 'F'
+        }
+        
+        return `You got a ${gradeLetter} (${percent}%)`
+
 }
 
-const studentOneGrade = studentGrade(15, 100)
-console.log(studentOneGrade)
+try {
+    const studentOneGrade = studentGrade("this should be a number", 100)
+    console.log(studentOneGrade)
+} catch (e) {
+    console.log(e.message)
+}
