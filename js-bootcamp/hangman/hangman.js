8af6a6da006a8d2b2d1c6e6eb4f96fe86b81b25e
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-12-10 09:00:51 +0000

    Finished Section 10 Lecture: 100

--- js-bootcamp/hangman/hangman.js
+++ js-bootcamp/hangman/hangman.js
@@ -1,66 +1,64 @@
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    this.status = 'playing'
-}
-
-Hangman.prototype.calculateStatus = function () {
-    // .every checks if all values in an array are in the array and returns true
-    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
-
-    if (this.remainingGuesses === 0) {
-        this.status = "failed"
-    } else if (finished) {
-        this.status = 'finished'
-    } else {
-        this.status = 'playing'
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
+        this.status = 'playing' 
     }
-}
+    calculateStatus() {
+        // .every checks if all values in an array are in the array and returns true
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
 
-Hangman.prototype.getStatusMessage = function () {
-    if (this.status === 'playing') {
-        return `Guesses left: ${this.remainingGuesses}.`
-    } else if (this.status === 'finished') {
-        return 'Great work! You guessed the word.'
-    } else {
-        return `Nice try! The word was "${this.word.join('')}".`
-    }
-}
-
-Hangman.prototype.getPuzzle = function () {
-    let puzzle = ''
-
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter
+        if (this.remainingGuesses === 0) {
+            this.status = "failed"
+        } else if (finished) {
+            this.status = 'finished'
         } else {
-            puzzle += '*'
+            this.status = 'playing'
         }
-    })
-
-    return puzzle
-}
-
-Hangman.prototype.makeGuess = function (guess) {
-    guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-
-    // This disables guesses if status is not 'playing' by returning nothing
-    // which stops the blocks below running
-    if (this.status !== 'playing') {
-        return
     }
-
-    if(isUnique) {
-       this.guessedLetters.push(guess) 
+    getStatusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.remainingGuesses}.`
+        } else if (this.status === 'finished') {
+            return 'Great work! You guessed the word.'
+        } else {
+            return `Nice try! The word was "${this.word.join('')}".`
+        }
     }
+    getPuzzle() {
+        let puzzle = ''
 
-    if (isUnique && isBadGuess) {
-        this.remainingGuesses--
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle += letter
+            } else {
+                puzzle += '*'
+            }
+        })
+
+        return puzzle
     }
+    makeGuess (guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
 
-    this.calculateStatus()
+        // This disables guesses if status is not 'playing' by returning nothing
+        // which stops the blocks below running
+        if (this.status !== 'playing') {
+            return
+        }
+
+        if(isUnique) {
+        this.guessedLetters.push(guess) 
+        }
+
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+
+        this.calculateStatus()
+        }
 }
 
--- js-bootcamp/oop/person.js
+++ js-bootcamp/oop/person.js
@@ -1,31 +1,64 @@
 // Prototypal Inheritance
 
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+
+        return bio
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
-
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-
-    return bio
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.position}.`
+    }
+    getYearsLeft() {
+        return 65 - this.age
+    }
 }
 
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+class Student extends Person {
+    constructor(firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    updateGrade(change) {
+    this.grade += change
+    }
+    getBio() {
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class.`
+    }
 }
 
-const me = new Person('Naruto', 'Uzamaki', 26, ['Teaching', 'Biking'])
-me.setName('Alexis Turner')
-console.log(me.getBio())
+const studentOne = new Student('Harry', 'Maguire', 16, 88)
+console.log(studentOne.getBio())
+studentOne.updateGrade(-30)
+console.log(studentOne.getBio())
 
-const person2 = new Person('Sasuke', 'Uchiwa', 27)
-console.log(person2.getBio())
+// const me = new Employee('Naruto', 'Uzamaki', 26, 'Teacher', ['Teaching', 'Biking'])
+// me.setName('Alexis Turner')
+// console.log(me.getBio())
+// console.log(me.getYearsLeft())
+
+// const person2 = new Person('Sasuke', 'Uchiwa', 27)
+// console.log(person2.getBio())
