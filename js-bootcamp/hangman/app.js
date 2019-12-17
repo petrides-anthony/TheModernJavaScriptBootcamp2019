f937bcf811c182d194f656f8968a5a332209fb33
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-12-17 08:46:21 +0000

    Finished Section 11: Lecture 107

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -1,7 +1,3 @@
-// HTTP
-// Request - What do we want to do
-// Response - What was actually done
-
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('Car Parts', 2)
@@ -17,17 +13,30 @@ window.addEventListener('keypress', (e) => {
 
 })
 
-// Making an HTTP request
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else if (e.target.readyState === 4) {
-        console.log('An error has taken place')
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
     }
 })
 
-request.open('GET' , 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+
+// Making an HTTP request
+
+// // New API Request for getting Country name based on given Country Code
+// const countryCode = "MX"
+// const countryApiRequest = new XMLHttpRequest()
+
+// countryApiRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((country) => country.alpha2Code === countryCode)
+//         console.log(country.name)
+//     } else if (e.target.readyState === 4) {
+//         console.log('An error has taken place')
+//     }
+// })
+
+// countryApiRequest.open('GET' , 'http://restcountries.eu/rest/v2/all')
+// countryApiRequest.send()
